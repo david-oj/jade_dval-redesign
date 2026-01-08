471d0e63d93ae387f2c5abd004ddbc08486de063
Eric <stephen.erich243@gmail.com>
2026-01-08 16:31:29 +0000

    fixed the issue

--- jadedval_backend/utils.js
+++ jadedval_backend/utils.js
@@ -7,7 +7,7 @@ export const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        secure: false,
+        secure: true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
