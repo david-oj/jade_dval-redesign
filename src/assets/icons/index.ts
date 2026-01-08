c6d4763f9c1ce1d6c4c9a4d1539b6718681f5a7a
david <davidojo.dev@gmail.com>
2026-01-08 16:55:54 +0100

    Add whatsapp link button to the  LandingPage

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,31 +1,33 @@
-import checkCircle from './checkCircle.svg';
-import partnerCircles from './partnerCircles.svg';
-import mail from './mail.svg';
-import telephone from './telephone.svg';
-import location from './location.svg';
-import facebook from './facebook.svg';
-import twitter from './twitter.svg';
-import linkedin from './linkedin.svg';
-import instagram from './instagram.svg';
-import whatsapp from './whatsapp.svg';
-import copyright from './copyright.svg';
-import star from './Star.svg';
+import checkCircle from "./checkCircle.svg";
+import partnerCircles from "./partnerCircles.svg";
+import mail from "./mail.svg";
+import telephone from "./telephone.svg";
+import location from "./location.svg";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import linkedin from "./linkedin.svg";
+import instagram from "./instagram.svg";
+import whatsapp from "./whatsapp.svg";
+import copyright from "./copyright.svg";
+import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import whatsappFill from "./whatsappFill.svg";
 
 export {
-    checkCircle,
-    partnerCircles,
-    mail,
-    telephone,
-    location,
-    facebook,
-    twitter,
-    linkedin,
-    instagram,
-    whatsapp,
-    copyright,
-    star,
-    menu,
-    close
-}
+  checkCircle,
+  partnerCircles,
+  mail,
+  telephone,
+  location,
+  facebook,
+  twitter,
+  linkedin,
+  instagram,
+  whatsapp,
+  copyright,
+  star,
+  menu,
+  close,
+  whatsappFill,
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,13 @@ import {
   Partner,
   EnrollNow,
 } from "@/sections";
+import WhatsappFill from "@/assets/icons/whatsappFill.svg?react";
 
 const LandingPage = () => {
   return (
     <div className="max-w-[1536px] mx-auto">
       <div className="bg-black max-h-screen overflow-hidden">
-        <CountDown/>
+        <CountDown />
         <Header />
         <Hero />
       </div>
@@ -25,6 +26,17 @@ const LandingPage = () => {
       <Partner />
       <JadeStarCTA />
       <EnrollNow />
+
+      {/* WHatsapp group link */}
+      <a
+        rel="noreferrer noopener "
+        target="_blank"
+        href="https://chat.whatsapp.com/DwmFC4pN6B52QAS7hQmhQD"
+      >
+        <button className="fixed bottom-8 right-5 z-50 size-10 sm:size-14 bg-secondary rounded-full flex items-center justify-center shadow-lg hover:shadow-xl hover:cursor-pointer">
+          <WhatsappFill className="size-6 sm:size-8 text-white" />
+        </button>
+      </a>
     </div>
   );
 };
